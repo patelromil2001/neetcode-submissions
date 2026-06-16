52a94af7019f5280e506c0e57c1e7acb9a99031c
Romil <69967165+patelromil2001@users.noreply.github.com>
2026-06-16 01:36:53 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count = {}
+    
+        # Count frequencies in the first string
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+            
+        # Decrement frequencies using the second string
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -= 1
+            
+        return True
+
+
+        
