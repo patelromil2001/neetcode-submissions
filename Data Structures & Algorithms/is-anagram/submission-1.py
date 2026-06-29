3ede7007913b95939cec13d1f70dd01396952169
Romil <69967165+patelromil2001@users.noreply.github.com>
2026-06-29 15:12:26 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count = {}
+        # Create a Frequency map of Characters in first string
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+            
+        # Decrement frequencies using the second string
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -= 1
+            
+        return True
+
+
+        
